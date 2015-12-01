cac761826e174e577a31b6e3c971defd22638d42
Mark <markhost@yandex.ru>
2015-12-01 15:26:20 +0300

    Initial commit

---
+++ testmadmike/spiders/mainspider.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import scrapy, json
+from scrapy.spiders import CrawlSpider
+from scrapy.http import Request
+
+
+class MainspiderSpider(CrawlSpider):
+    name = "mainspider"
+    allowed_domains = ["appointmentpro.net"]
+    start_urls = (
+        'http://appointmentpro.net/',
+    )
+
+    def parse(self, response):
+        pass
