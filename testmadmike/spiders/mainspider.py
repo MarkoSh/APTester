cd64525f6bf6dfb16ecf85a528f14f04b3d183f2
Mark <markhost@yandex.ru>
2015-12-02 01:29:51 +0300

    Вынес строку даты в отдельную переменную для удобства

--- testmadmike/spiders/mainspider.py
+++ testmadmike/spiders/mainspider.py
@@ -44,18 +44,19 @@ class MainspiderSpider(CrawlSpider):
             log.error('Response status {}'.format(response.status))
 
 class logger():
+    timeformat = '%Y-%m-%d %H:%M:%S'
     def info(self, msg='Information'):
-        line = 'INFORMATION: {} - {}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), msg)
+        line = 'INFORMATION: {} - {}'.format(datetime.now().strftime(self.timeformat), msg)
         print(colors.yellow(line))
         self.write('log', line)
 
     def success(self, msg='Success'):
-        line = 'SUCCESS: {} - {}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), msg)
+        line = 'SUCCESS: {} - {}'.format(datetime.now().strftime(self.timeformat), msg)
         print(colors.green(line))
         self.write('log', line)
 
     def error(self, msg='Error'):
-        line = 'ERROR: {} - {}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), msg)
+        line = 'ERROR: {} - {}'.format(datetime.now().strftime(self.timeformat), msg)
         print >> sys.stderr, colors.red(line)
         self.write('log', line)
 
