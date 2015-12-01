999530e2905f194c05362be59247ef549ec6edd4
Mark <markhost@yandex.ru>
2015-12-01 18:13:58 +0300

    Проверка статуса дэшборда

--- testmadmike/spiders/mainspider.py
+++ testmadmike/spiders/mainspider.py
@@ -1,15 +1,30 @@
 # -*- coding: utf-8 -*-
-import scrapy, json
+import scrapy, json, colors
 from scrapy.spiders import CrawlSpider
 from scrapy.http import Request
+from datetime import datetime
+
 
 
 class MainspiderSpider(CrawlSpider):
     name = "mainspider"
-    allowed_domains = ["appointmentpro.com"]
+    allowed_domains = ["http://api-dot-bizzbook-app.appspot.com/"]
     start_urls = (
-        'http://appointmentpro.com/',
+        'https://api-dot-bizzbook-app.appspot.com/v2/dashboard/stats',
     )
+    # paths = [path.strip() for path in open('paths.txt', 'r').readlines()]
 
     def parse(self, response):
+        log = logger()
+        data = json.loads(response.body)
+        if data['status'] == 'success':
+            log.success(data['message'])
+        else:
+            log.error(data['message'])
         pass
+
+class logger():
+    def success(self, msg='Success'):
+        print('{} - {}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), colors.green(msg)))
+    def error(self, msg='Error'):
+        print('{} - {}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), colors.red(msg)))
