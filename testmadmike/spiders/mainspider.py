c18e03ecc27aabeecc55084c5e33eef2b49a628d
Mark <markhost@yandex.ru>
2015-12-02 00:45:22 +0300

    Реализовал проверку по списку путей Исключим неиспользуемые пути и пути с изменяемыми участками

--- testmadmike/spiders/mainspider.py
+++ testmadmike/spiders/mainspider.py
@@ -1,30 +1,63 @@
 # -*- coding: utf-8 -*-
-import scrapy, json, colors
+import scrapy, json, colors, sys
 from scrapy.spiders import CrawlSpider
 from scrapy.http import Request
 from datetime import datetime
-
-
 
 class MainspiderSpider(CrawlSpider):
     name = "mainspider"
     allowed_domains = ["http://api-dot-bizzbook-app.appspot.com/"]
     start_urls = (
-        'https://api-dot-bizzbook-app.appspot.com/v2/dashboard/stats',
+        'http://api-dot-bizzbook-app.appspot.com/v2/dashboard/stats',
     )
-    # paths = [path.strip() for path in open('paths.txt', 'r').readlines()]
+    handle_httpstatus_list = [404, 500]
+    paths = [path.strip() for path in open('paths.txt', 'r').readlines() if 'cron' not in path]
 
     def parse(self, response):
         log = logger()
-        data = json.loads(response.body)
-        if data['status'] == 'success':
-            log.success(data['message'])
+        log.info('Url {} available'.format(response.url))
+        try:
+            data = json.loads(response.body)
+            if 'status' in data and data['status'] == 'success':
+                log.success(data['message'])
+            else:
+                log.error(data['message'])
+            for path in self.paths:
+                yield Request(url='http://api-dot-bizzbook-app.appspot.com{}'.format(path), callback=self.parse_, dont_filter=True)
+        except ValueError as e:
+            log.error(e)
+
+    def parse_(self, response):
+        log = logger()
+        if response.status == 200:
+            log.info('Url {} available'.format(response.url))
+            try:
+                data = json.loads(response.body)
+                if 'status' in data and data['status'] == 'success':
+                    log.success(data['message'])
+                else:
+                    log.error(data['message'])
+            except ValueError as e:
+                log.error(e)
         else:
-            log.error(data['message'])
-        pass
+            log.error('Url {} not available'.format(response.url))
+            log.error('Response status {}'.format(response.status))
 
 class logger():
+    def info(self, msg='Information'):
+        line = 'INFORMATION: {} - {}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), msg)
+        print(colors.yellow(line))
+        self.write('log', line)
+
     def success(self, msg='Success'):
-        print('{} - {}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), colors.green(msg)))
+        line = 'SUCCESS: {} - {}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), msg)
+        print(colors.green(line))
+        self.write('log', line)
+
     def error(self, msg='Error'):
-        print('{} - {}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), colors.red(msg)))
+        line = 'ERROR: {} - {}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), msg)
+        print >> sys.stderr, colors.red(line)
+        self.write('log', line)
+
+    def write(self, type, line):
+        open('{}.log'.format(type), 'a').write('{}\n'.format(line))
