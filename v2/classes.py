0c2b7d715ca4a61eeb803b5b4645dadf7bc7fa7d
Mark <markhost@yandex.ru>
2015-12-07 17:30:12 +0300

    Бакап

--- v2/classes.py
+++ v2/classes.py
@@ -105,22 +105,22 @@ class Tester():
             link = '{}{}'.format(self.host, path['path'])
             self.log.info('{}, function: {}'.format(link, path['func']))
             with Profiler() as p:
-                # if func == 'checkStatus':
-                #     self.checkStatus(path=path)
-                # if func == 'testUser':
-                #     self.testUser(path=path)
-                # if func == 'authUser':
-                #     self.authUser(path=path)
+                if func == 'checkStatus':
+                    self.checkStatus(path=path)
+                if func == 'testUser':
+                    self.testUser(path=path)
+                if func == 'authUser':
+                    self.authUser(path=path)
                 # if func == 'testLocation':
                 #     self.testLocation(path=path)
                 # if func == 'getStats':
                 #     self.getStats(path=path)
-                if func == 'sendMessage':
-                    self.sendMessage(path=path)
-                if func == 'getMessages':
-                    self.getMessages(path=path)
-                if func == "readConversation":
-                    self.readConversation(path=path)
+                # if func == 'sendMessage':
+                #     self.sendMessage(path=path)
+                # if func == 'getMessages':
+                #     self.getMessages(path=path)
+                # if func == "readConversation":
+                #     self.readConversation(path=path)
                 # if func == 'ziptoloc':
                 #     self.ziptoloc(path=path)
                 # if func == 'createCustomer':
@@ -355,7 +355,7 @@ class Tester():
     def readConversation(self, path):
         link = '{}{}'.format(self.host, path['path'])
         self.log.info('Requesting conversation...')
-        random.shuffle(self.users)
+        # random.shuffle(self.users)
         for user in self.users:
             with Profiler() as p:
                 link_ = '{}/v2/user/messages?user_id={}'.format(self.host, user['key']['id'])
